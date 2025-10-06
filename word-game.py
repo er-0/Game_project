b3a12488435776cf18ba1b9d6f84b2e86604c7c9
linnea <linneagodunov@protonmail.com>
2025-10-06 13:23:55 +0300

    mitä

--- word-game.py
+++ word-game.py
@@ -1,12 +1,18 @@
+import random
+def random_letters():
+    letters = "ABCDEFGHIJKLMNOPRSTUVYÄÖ"
+    return random.sample(letters, 8)
+
+
+
 finnish = open("every-finnish-word.txt", encoding="utf-8")
-finnish_words = [word.strip().lower() for word in finnish]
+finnish_words = [word.strip() for word in finnish]
 finnish.close()
 
 def is_it_finnish(word):
     return word.lower() in finnish_words
 
 #testi
-
 while True:
     word = input("anna sana: ")
     if not word:
