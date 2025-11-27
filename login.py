0fa747f088360fe7d2cb6b9ad744ebac54744b75
ElizavetaZhogol <liza.zhogol@gmail.com>
2025-11-27 17:37:11 +0300

    login/register

--- login.py
+++ login.py
@@ -13,6 +13,37 @@ def web_check_user_exists(username):
     count = kursori.fetchone()[0]
     return count > 0 
 
+def web_register_user(name):
+    if web_check_user_exists(name):
+        return False
+    sql = f"SELECT ident FROM game_airports ORDER BY RAND() LIMIT 1;"
+    kursori = yhteys.cursor()
+    kursori.execute(sql)
+    ident = kursori.fetchone()[0]
+
+    sql = f"SELECT COUNT(*) FROM players WHERE starting_airport = %s;"
+    kursori = yhteys.cursor()
+    kursori.execute(sql, (ident,))
+    count_airport = kursori.fetchone()[0]
+
+    while count_airport != 0:
+        sql = f"SELECT ident FROM game_airports ORDER BY RAND() LIMIT 1;"
+        kursori = yhteys.cursor()
+        kursori.execute(sql)
+        ident = kursori.fetchone()[0]
+
+        sql = f"SELECT COUNT(*) FROM players WHERE starting_airport = %s;"
+        kursori = yhteys.cursor()
+        kursori.execute(sql, (ident,))
+        count_airport = kursori.fetchone()[0]
+
+    sql = f"INSERT INTO players (user_name, starting_airport) VALUES (%s, %s);"
+    kursori = yhteys.cursor()
+    kursori.execute(sql, (name, ident))
+    yhteys.commit()
+
+    return True
+
 app = Flask(__name__)
 app.secret_key = secrets.token_hex(16)
 
@@ -31,6 +62,17 @@ def login_page():
         return jsonify({"success": True, "message": "Login successful"})
     else:
         return jsonify({"success": False, "message": "User not found"})
+    
+@app.route("/register", methods=["POST"])
+def register():
+    data = request.get_json()
+    username = data.get("username")
+    
+    if web_register_user(username):     
+        session['user_name'] = username  
+        return jsonify({"success": True, "message": "Registration successful"})
+    else:
+        return jsonify({"success": False, "message": "Username already exists"})
 
 
 if __name__ == "__main__":
--- static/login.js
+++ static/login.js
@@ -1,3 +1,5 @@
+
+// actions with login form
 const loginForm = document.getElementById('loginForm');
 
 loginForm.addEventListener('submit', async function (evt) {
@@ -13,6 +15,30 @@ loginForm.addEventListener('submit', async function (evt) {
     const result = await response.json();
 
     if (result.success) {
+        console.log(result)
+    }
+    else {
+        console.log('No such user');
+    }
+});
+
+// actions with registration form
+const registrationForm = document.getElementById('registerForm');
+
+registrationForm.addEventListener('submit', async function (evt) {
+    evt.preventDefault();
+
+    const username = document.getElementById('registerUsername').value;
+
+    const response = await fetch('/register', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ username: username }),
+    });
+
+    const result = await response.json();
+
+    if (result) {
         console.log(result)
     }
 });
