906f6f7d412f7bccc06a3fcf6a6887de341e0d46
er-0 <95697943+er-0@users.noreply.github.com>
2025-11-28 10:07:26 +0200

    Login split into two files

--- login.py
+++ login.py
@@ -2,59 +2,7 @@ from flask import Flask, jsonify, request, session, render_template
 import secrets
 import os
 
-from db import yhteys
-
-# login
-def web_check_user_exists(username):
-
-    sql = f"SELECT COUNT(*) FROM players WHERE user_name = %s;"
-    kursori = yhteys.cursor()
-    kursori.execute(sql, (username,))
-    count = kursori.fetchone()[0]
-    return count > 0 
-
-# register new user
-def web_register_user(name):
-    if web_check_user_exists(name):
-        return False
-    sql = f"SELECT ident FROM game_airports ORDER BY RAND() LIMIT 1;"
-    kursori = yhteys.cursor()
-    kursori.execute(sql)
-    ident = kursori.fetchone()[0]
-
-    sql = f"SELECT COUNT(*) FROM players WHERE starting_airport = %s;"
-    kursori = yhteys.cursor()
-    kursori.execute(sql, (ident,))
-    count_airport = kursori.fetchone()[0]
-
-    while count_airport != 0:
-        sql = f"SELECT ident FROM game_airports ORDER BY RAND() LIMIT 1;"
-        kursori = yhteys.cursor()
-        kursori.execute(sql)
-        ident = kursori.fetchone()[0]
-
-        sql = f"SELECT COUNT(*) FROM players WHERE starting_airport = %s;"
-        kursori = yhteys.cursor()
-        kursori.execute(sql, (ident,))
-        count_airport = kursori.fetchone()[0]
-
-    sql = f"INSERT INTO players (user_name, starting_airport) VALUES (%s, %s);"
-    kursori = yhteys.cursor()
-    kursori.execute(sql, (name, ident))
-    yhteys.commit()
-
-    return True
-
-# get player information
-def player_information(name):
-    sql = (f"SELECT a.ident, a.name, a.latitude_deg, a.longitude_deg, a.continent, a.municipality, a.country_name, "
-           f"p.id, p.games_played, p.last_game FROM game_airports AS a INNER JOIN players AS p ON p.starting_airport "
-           f"= a.ident where p.user_name = %s;")
-    kursori = yhteys.cursor()
-    kursori.execute(sql, (name,))
-    users_information = kursori.fetchall()
-
-    return users_information
+from loginfunctions import web_register_user, web_check_user_exists, player_information
 
 app = Flask(__name__)
 app.secret_key = secrets.token_hex(16)
